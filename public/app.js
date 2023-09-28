bc3d7bf6f06c81135fe66a765f4717f252c0c107
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-09-28 17:56:26 +0300

    Deleted coments

--- public/app.js
+++ public/app.js
@@ -2,26 +2,7 @@
 import { ListTemplate } from "./classes/ListTemplate.js";
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/payments.js";
-const me = {
-    name: 'Shaun',
-    age: 30,
-    speak(text) {
-        console.log(text);
-    }, spend(amount) {
-        console.log(`I spent`, amount);
-        return amount;
-    }
-};
-// console.log(me);
-// const firstInvoice = new Invoice('Ann', 'accounting services', 200);
-// const secondInvoice = new Invoice('Marie', 'peanut butter', 300);
-// console.log(firstInvoice.format());
-// let invoices: Invoice [] = [];
-// invoices.push(firstInvoice);
-// invoices.push(secondInvoice);
-// invoices.forEach(invoice => console.log(invoice.client, invoice.format()));
 const form = document.querySelector('.new-item-form');
-//console.log(form.children);
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -4,43 +4,7 @@ import { Invoice } from "./classes/invoice.js";
 import {Payment} from "./classes/payments.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-interface IsPerson {
-    name: string;
-    age: number;
-    speak(a:string):void;
-    spend(a:number):number;
-}
-
-const me: IsPerson = {
-    name: 'Shaun',
-    age : 30,
-    speak (text: string):void {
-        console.log(text);
-    }, spend (amount: number):number {
-        console.log(`I spent`, amount);
-        return amount;
-    }
-}
-
-// console.log(me);
-
-
-// const firstInvoice = new Invoice('Ann', 'accounting services', 200);
-// const secondInvoice = new Invoice('Marie', 'peanut butter', 300);
-// console.log(firstInvoice.format());
-
-// let invoices: Invoice [] = [];
-// invoices.push(firstInvoice);
-// invoices.push(secondInvoice);
-
-// invoices.forEach(invoice => console.log(invoice.client, invoice.format()));
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
-//console.log(form.children);
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
